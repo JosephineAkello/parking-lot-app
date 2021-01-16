5cea8299c4a0d55e0e1b44df542843e34ce72758
JosephineAkello <josephineakello2030@gmail.com>
2021-01-16 23:05:48 +0300

    initial commit

---
+++ src/main/java/com/example/parkinglotapp/ParkingLotAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.parkinglotapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingLotAppApplication.class, args);
+		System.out.println("Helllo spring parking lot");
+	}
+
+}

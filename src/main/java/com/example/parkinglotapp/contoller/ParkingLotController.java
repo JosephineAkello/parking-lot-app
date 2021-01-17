c9a8d740ba7e1d7000cc1e3027d22d569033168f
JosephineAkello <josephineakello2030@gmail.com>
2021-01-17 18:05:31 +0300

    implement swagger ui

--- src/main/java/com/example/parkinglotapp/contoller/ParkingLotController.java
+++ src/main/java/com/example/parkinglotapp/contoller/ParkingLotController.java
@@ -3,6 +3,7 @@ package com.example.parkinglotapp.contoller;
 import com.example.parkinglotapp.model.MyIntroduction;
 import lombok.Builder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,4 +14,10 @@ public class ParkingLotController {
     public MyIntroduction aboutMe(){
         return  MyIntroduction.builder().name("Jos").age(10).skills("Java -Spring").build();
     }
+
+    @PostMapping("/infoAboutSomeone")
+    public MyIntroduction infoAboutSomeone(String name, int age, String skills){
+        return  MyIntroduction.builder().name(name).age(age).skills(skills).build();
+    }
+
 }
